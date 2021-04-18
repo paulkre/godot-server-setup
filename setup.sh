c81d9149cd7702becdb93f592f069283a7d92f9c
Paul Kretschel <paul.kretschel@icloud.com>
2021-04-19 01:58:59 +0200

    Fix script locations

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ wget https://downloads.tuxfamily.org/godotengine/3.2.3/Godot_v3.2.3-stable_linux
 unzip Godot_v3.2.3-stable_linux_server.64.zip && rm Godot_v3.2.3-stable_linux_server.64.zip
 sudo mv Godot_v3.2.3-stable_linux_server.64 /usr/local/bin/godot
 
-cp start-server.sh $HOME
+cp start-server.sh /usr/src
 
 sudo cp game.service /etc/systemd/system
 sudo systemctl daemon-reload
--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
-PCK_FILE="$HOME/data.pck"
+PCK_FILE=/usr/src/data.pck
 
 if [ -f "$PCK_FILE" ]; then
   godot --main-pack $PCK_FILE --server
